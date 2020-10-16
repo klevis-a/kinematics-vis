6d20c74c2d0a9c6492760ab77c56a126064bc3cd
Klevis <klevis.aliaj@utah.edu>
2020-10-16 17:00:16 -0600

    Changing the default trial for now

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function loadPapaParse() {
 }
 
 const csvPathParam = get_url_param('csvpath');
-const csvPath =  './csv/'.concat(csvPathParam ? csvPathParam : 'N002A_CA_t01.csv');
+const csvPath =  './csv/'.concat(csvPathParam ? csvPathParam : 'N005_CA_t01.csv');
 const csvLoaderInit = loadPapaParse().then(papa => new CSVLoader(papa));
 const landmarkInit = csvLoaderInit.then(csvLoader => csvLoader.loadCsv('./csv/N005_CTdata_Input_for_mtwtesla.csv'));
 const timeSeriesCsvInit = csvLoaderInit.then((csvLoader) => csvLoader.loadCsv(csvPath));
