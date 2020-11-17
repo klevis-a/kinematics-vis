685fcfe98a2326ae4c6851f77f4a54b664b1b7e1
Klevis <klevis.aliaj@utah.edu>
2020-11-17 13:04:28 -0700

    Switched to using palette vs palette.min because it was throwing DevTools errors.

--- downloadDeps.bat
+++ downloadDeps.bat
@@ -7,7 +7,7 @@ mkdir js\vendor\three.js\examples\jsm\controls
 mkdir js\vendor\three.js\examples\jsm\utils
 mkdir js\vendor\three.js\examples\jsm\loaders
 mkdir js\vendor\three.js\examples\jsm\libs
-curl https://cdn.jsdelivr.net/npm/google-palette@1.1.0/palette.min.js --output js\vendor\palette.min.js
+curl https://cdn.jsdelivr.net/npm/google-palette@1.1.0/palette.js --output js\vendor\palette.js
 curl https://cdn.jsdelivr.net/npm/papaparse@5.3.0/papaparse.min.js --output js\vendor\papaparse.min.js
 curl https://cdn.plot.ly/plotly-1.57.0.min.js --output js\vendor\plotly.min.js
 curl https://cdn.jsdelivr.net/npm/svd-js@1.1.1/build-es/svd.js --output js\vendor\svd.js
